48970420419c5489f039d17303f9c3c1d1339430
hao <1635920137@qq.com>
2017-10-17 18:29:19 +0800

    添加jwt实现工具类

---
+++ src/main/test/com/jin321/pl/utils/JWTUtilTest.java
@@ -0,0 +1,98 @@
+package com.jin321.pl.utils;
+
+import com.jin321.wx.model.LoginEntity;
+import io.jsonwebtoken.Claims;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author hao
+ * @Date 2017/10/17 17:05
+ * @Description :
+ */
+public class JWTUtilTest {
+    @Test
+    public void parseJWT1() throws Exception {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "zhangxin");
+        map.put("age", "1");
+        map.put("sex", "women");
+        map.put("number", "asnkdas");
+        String jwt = JWTUtil.createJWT(map,1);
+        System.out.println(jwt);
+        Claims claims = JWTUtil.parseJWT(jwt);
+        System.out.println(claims);
+    }
+
+    @Test
+    public void parseJWTToMap() throws Exception {
+        LoginEntity loginEntity = new LoginEntity();
+        loginEntity.setUserid("sladasdasdsad");
+        loginEntity.setOpenid("jsadfkdjfkl");
+        loginEntity.setSessionKey("jfklsajkl");
+        loginEntity.setTim(1232345);
+        String jwt = JWTUtil.createJWTfromBean(loginEntity);
+        System.out.println(jwt);
+        Map<String, Object> map = JWTUtil.parseJWTToMap(jwt);
+        System.out.println(map);
+    }
+
+    @Test
+    public void parseJWTToBean() throws Exception {
+    }
+
+    @Test
+    public void createJWT1() throws Exception {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "zhangxin");
+        map.put("age", "1");
+        map.put("sex", "women");
+        map.put("number", "asnkdas");
+        String jwt = JWTUtil.createJWT(map);
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void createJWT2() throws Exception {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "zhangxin");
+        map.put("age", "1");
+        map.put("sex", "women");
+        map.put("number", "asnkdas");
+        String jwt = JWTUtil.createJWT(map,1);
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void createJWT3() throws Exception {
+        LoginEntity loginEntity = new LoginEntity();
+        loginEntity.setUserid("sladasdasdsad");
+        loginEntity.setOpenid("jsadfkdjfkl");
+        loginEntity.setSessionKey("jfklsajkl");
+        loginEntity.setTim(1232345);
+        String jwt = JWTUtil.createJWTfromBean(loginEntity);
+        System.out.println(jwt);
+    }
+
+    @Test
+    public void parseJWT() throws Exception {
+        LoginEntity loginEntity = new LoginEntity();
+        loginEntity.setUserid("sladasdasdsad");
+        loginEntity.setOpenid("jsadfkdjfkl");
+        loginEntity.setSessionKey("jfklsajkl");
+        loginEntity.setTim(1232345);
+        String jwt = JWTUtil.createJWTfromBean(loginEntity);
+        System.out.println(jwt);
+        LoginEntity loginEntity1 = JWTUtil.parseJWTToBean(jwt, loginEntity);
+        System.out.println(loginEntity);
+
+    }
+
+    @Test
+    public void createJWT() throws Exception {
+
+    }
+
+}
--- src/main/test/com/jin321/wx/utils/AESTest.java
+++ src/main/test/com/jin321/wx/utils/AESTest.java
@@ -1,6 +1,5 @@
 package com.jin321.wx.utils;
 
-import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 
 /**
@@ -16,17 +15,18 @@ public class AESTest {
 //        String sessionKey = "tY2vqkZeFNdeS/KFBWaoGg==";
 //        String iv = "0atCNV08BHZQwd4WwqKDqw==";
 
-        String appId = "wx4f4bc4dec97d474b";
-        String sessionKey = "tiihtNczf5v6AKRyjwEUhQ==";
-        String encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew==";
-        String iv = "r7BXXKkLb8qrSNn05n0qiA==";
-        AES aes = new AES();
-        byte[] resultByte = aes.decrypt(Base64.decodeBase64(encryptedData), Base64.decodeBase64(sessionKey), Base64.decodeBase64(iv));
-        String userInfo = new String(resultByte, "UTF-8");
-        System.out.println(userInfo);
-        System.out.println(sessionKey.length());
-        System.out.println(encryptedData.length());
-        System.out.println(iv.length());
+//        String appId = "wx4f4bc4dec97d474b";
+//        String sessionKey = "tiihtNczf5v6AKRyjwEUhQ==";
+//        String encryptedData = "CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZMQmRzooG2xrDcvSnxIMXFufNstNGTyaGS9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+3hVbJSRgv+4lGOETKUQz6OYStslQ142dNCuabNPGBzlooOmB231qMM85d2/fV6ChevvXvQP8Hkue1poOFtnEtpyxVLW1zAo6/1Xx1COxFvrc2d7UL/lmHInNlxuacJXwu0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn/Hz7saL8xz+W//FRAUid1OksQaQx4CMs8LOddcQhULW4ucetDf96JcR3g0gfRK4PC7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns/8wR2SiRS7MNACwTyrGvt9ts8p12PKFdlqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYVoKlaRv85IfVunYzO0IKXsyl7JCUjCpoG20f0a04COwfneQAGGwd5oa+T8yO5hzuyDb/XcxxmK01EpqOyuxINew==";
+//        String iv = "r7BXXKkLb8qrSNn05n0qiA==";
+//        AES aes = new AES();
+//        byte[] resultByte = aes.decrypt(Base64.decodeBase64(encryptedData), Base64.decodeBase64(sessionKey), Base64.decodeBase64(iv));
+//        String userInfo = new String(resultByte, "UTF-8");
+//        System.out.println(userInfo);
+//        System.out.println(sessionKey.length());
+//        System.out.println(encryptedData.length());
+//        System.out.println(iv.length());
+
     }
 
 }
