7aef9e95d079532d0bced658499c51df888a1788
tyranitarx <tyranitarx@163.com>
2017-12-21 20:25:04 +0800

    添加接收支付信息接口日志

--- src/main/java/com/jin321/ms/controller/RecievePayMessageController.java
+++ src/main/java/com/jin321/ms/controller/RecievePayMessageController.java
@@ -3,6 +3,8 @@ package com.jin321.ms.controller;
 import com.jin321.ms.Service.OrderFormService;
 import com.jin321.ms.model.PayReturnMessage;
 import com.jin321.ms.model.RecieveMessage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +22,7 @@ import java.util.Date;
 @Controller
 @RequestMapping("/ms")
 public class RecievePayMessageController {
+    private static final Log log = LogFactory.getLog(RecievePayMessageController.class);
     @Autowired
     private OrderFormService orderFormService;
     private PayReturnMessage payReturnMessage;
@@ -28,6 +31,7 @@ public class RecievePayMessageController {
     @ResponseBody
     public PayReturnMessage getReturnMessage(@RequestBody RecieveMessage recieveMessage)throws Exception{
         payReturnMessage=new PayReturnMessage();
+        log.info("从微信获取到的信息为:"+recieveMessage);
         if(recieveMessage.getResult_code().equals("SUCCESS")){
             sign=orderFormService.changeOrderFormStatue(1,Long.parseLong(recieveMessage.getOut_trade_no()),new Date());
             if(sign==1){
