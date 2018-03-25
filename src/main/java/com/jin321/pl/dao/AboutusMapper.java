73b873fa0dd8dee1e063b5b3aba43318b9772933
TyranitarX <tyranitarx@163.com>
2018-03-25 20:31:08 +0800

    修改数据库

---
+++ src/main/java/com/jin321/pl/dao/AboutusMapper.java
@@ -0,0 +1,34 @@
+package com.jin321.pl.dao;
+
+import com.jin321.pl.model.Aboutus;
+import com.jin321.pl.model.AboutusExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AboutusMapper {
+    int countByExample(AboutusExample example);
+
+    int deleteByExample(AboutusExample example);
+
+    int deleteByPrimaryKey(Integer auid);
+
+    int insert(Aboutus record);
+
+    int insertSelective(Aboutus record);
+
+    List<Aboutus> selectByExampleWithBLOBs(AboutusExample example);
+
+    List<Aboutus> selectByExample(AboutusExample example);
+
+    Aboutus selectByPrimaryKey(Integer auid);
+
+    int updateByExampleSelective(@Param("record") Aboutus record, @Param("example") AboutusExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Aboutus record, @Param("example") AboutusExample example);
+
+    int updateByExample(@Param("record") Aboutus record, @Param("example") AboutusExample example);
+
+    int updateByPrimaryKeySelective(Aboutus record);
+
+    int updateByPrimaryKeyWithBLOBs(Aboutus record);
+}
---
+++ src/main/java/com/jin321/pl/model/Aboutus.java
@@ -0,0 +1,23 @@
+package com.jin321.pl.model;
+
+public class Aboutus {
+    private Integer auid;
+
+    private byte[] detail;
+
+    public Integer getAuid() {
+        return auid;
+    }
+
+    public void setAuid(Integer auid) {
+        this.auid = auid;
+    }
+
+    public byte[] getDetail() {
+        return detail;
+    }
+
+    public void setDetail(byte[] detail) {
+        this.detail = detail;
+    }
+}
